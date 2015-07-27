ef7973dc0cf4d53389b92672785ecdbde95e0d33
luisangel584 <Pixculin13>
2015-07-27 14:01:58 -0500

    Guardando registros en la base de datos
    Ahora solo me gustaría poner algún tipo de alerta para confirmar de
    guardado. Por lo pronto el que los campos se limpien es señal de que se
    ha guardado, la url de la base de datos es
    https://listatareas-lcouoh.firebaseio.com/

---
+++ js/registro.js
@@ -0,0 +1,38 @@
+angular.module('registerApp', ['firebase'])
+
+.controller('RegisterCtrl', ['$scope', '$firebaseArray',
+    function($scope, $firebaseArray){
+
+        var ref = new Firebase('https://listatareas-lcouoh.firebaseio.com/registros');
+
+        $scope.registros = $firebaseArray(ref);
+
+        $scope.agregarRegistro = function(){
+            $scope.registros.$add({
+                nombre: $scope.nombre,
+                apellido_paterno: $scope.apellidop,
+                apellido_materno:$scope.apellidom,
+                nacimiento_dia: $scope.nacimientod,
+                nacimiento_mes: $scope.nacimientom,
+                nacimiento_anio: $scope.nacimientoa,
+                telefono: $scope.telefono,
+                email: $scope.email,
+                nivel_estudios: $scope.nivel,
+                puesto: $scope.puesto
+            });
+
+            $scope.nombre = "";
+            $scope.apellidop = "";
+            $scope.apellidom = "";
+            $scope.nacimientod = "";
+            $scope.nacimientom = "";
+            $scope.nacimientoa = "";
+            $scope.telefono = "";
+            $scope.email = "";
+            $scope.nivel = "";
+            $scope.puesto = "";
+
+        }
+
+
+    }]);
